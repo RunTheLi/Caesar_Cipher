32d9d78f5ae7a6e773d2abf65396374546ae2d92
RunTheLi <lanmarock@gmail.com>
2025-01-27 15:31:42 +0700

    feat: add rubocop Gem and gemlock

--- Ceasar_Cipher.rb
+++ Ceasar_Cipher.rb
@@ -2,11 +2,11 @@ def caesar_cipher(str, shift)
   result = ""
 
   str.each_char do |char|
-    if char.between?('a', 'z')
-      shift_char = ((char.ord - 'a'.ord + shift) % 26 + 'a'.ord).chr
+    if char.between?("a", "z")
+      shift_char = (((char.ord - "a".ord + shift) % 26) + "a".ord).chr
       result += shift_char
-    elsif char.between?('A', 'Z')
-      shift_char = ((char.ord - 'A'.ord + shift) % 26 + 'A'.ord).chr
+    elsif char.between?("A", "Z")
+      shift_char = (((char.ord - "A".ord + shift) % 26) + "A".ord).chr
       result += shift_char
     else
       result += char
